433142b7e170195a4f2b035af995cbbd660afeb7
Shai Solan <shaisolan@gmai.com>
2025-07-17 15:22:42 +0100

    Add CI/CD pipeline with tests

---
+++ math.test.js
@@ -0,0 +1,9 @@
+const { add, multiply } = require('./math');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('multiplies 3 * 4 to equal 12', () => {
+    expect(multiply(3, 4)).toBe(12);
+});
