a217d1ca4b6ff2e5fe538b7f1081d180714f73d8
Shai Solan <shaisolan@gmai.com>
2025-07-18 12:17:48 +0100

    IaC scan test 2

--- math.test.js
+++ math.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
-test('multiplies 3 * 4 so that it equals 12', () => {
+test('multiplies 3 * 4 so that it equals 12 test', () => {
     expect(multiply(3, 4)).toBe(12);
 });
